3c2297838a49f01db22f26104727a1b8e3f65ec2
Juan Vizcaino <elvizcacho@gmail.com>
2021-04-11 16:26:28 +0200

    feat(test): add readme

--- src/index.ts
+++ src/index.ts
@@ -18,14 +18,56 @@ engine.addRule({
     ],
   },
   event: {
-    type: "allConditionsAreMetEvent",
+    type: "onSupportedDocumentType",
     params: {
       message: "Document is supported and email contains @evana.de",
     },
   },
 });
 
+engine.addRule({
+  conditions: {
+    any: [
+      {
+        fact: "securityClassification",
+        operator: "equal",
+        value: "unrestricted",
+      },
+      {
+        fact: "securityClassification",
+        operator: "equal",
+        value: "",
+      },
+    ],
+  },
+  event: {
+    type: "onUnrestrictedDocument",
+    params: {
+      message: "document is unrestricted do something aobut it",
+    },
+  },
+});
+
+engine.addRule({
+  conditions: {
+    any: [
+      {
+        fact: "securityClassification",
+        operator: "equal",
+        value: "restricted",
+      },
+    ],
+  },
+  event: {
+    type: "onRestrictedDocument",
+    params: {
+      message: "document is restricted do something about it",
+    },
+  },
+});
+
 let facts = {
+  securityClassification: "unrestricted",
   documentMimeType: "pptx",
   uploaderEmail: "s.vizcaino@evana.de",
 };
@@ -41,11 +83,33 @@ engine.addOperator("contains", (fact: string, jsonValue: string) => {
 });
 
 engine.run(facts).then(({ events }) => {
-  const allConditionsAreMetEvent = events.find(
-    (event) => event.type === "allConditionsAreMetEvent"
+  const onSupportedDocumentType = events.find(
+    (event) => event.type === "onSupportedDocumentType"
   );
 
-  if (allConditionsAreMetEvent) {
-    console.log(allConditionsAreMetEvent.params);
+  const onRestrictedDocument = events.find(
+    (event) => event.type === "onRestrictedDocument"
+  );
+
+  const onUnrestrictedDocument = events.find(
+    (event) => event.type === "onUnrestrictedDocument"
+  );
+
+  if (onSupportedDocumentType && onRestrictedDocument) {
+    console.log(onSupportedDocumentType.params);
+    console.log(onRestrictedDocument.params);
+
+    console.log(
+      "document type is supported and is a restricted document to something"
+    );
+  }
+
+  if (onSupportedDocumentType && onUnrestrictedDocument) {
+    console.log(onSupportedDocumentType.params);
+    console.log(onUnrestrictedDocument.params);
+
+    console.log(
+      "document type is supported and is a unrestricted document to something"
+    );
   }
 });
